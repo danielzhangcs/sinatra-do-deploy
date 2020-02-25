ecf7f17dff9f715f6af62f2dbad2102e591071d2
danielzhangcs <danielzhang@brandeis.edu>
2020-02-25 18:36:44 -0500

    update

--- frontend-sinatra/app.rb
+++ frontend-sinatra/app.rb
@@ -7,24 +7,24 @@ require 'json'
 #   settings.api => "someaddress.ondigitalocean.com"
 set :api, ENV['API_HOST']
 set :api_port, ENV['API_PORT'] || 80
-root = '157.245.113.67/users'
-uri = URI(root)
+
 
 # Show a pretty table of all users... rendered on a html page.
 get '/' do
-  res =  Net::HTTP.get_response(ENV['API_HOST'] + "/users")
+  res =  Net::HTTP.get_response(settings.api, '/users', settings.api_port)
   @users = JSON.parse(res.body)
   erb :index
 end
 
 post '/users' do
-  Net::HTTP.post_form(ENV['API_HOST'] + "/users", {"n" => params["n"]})
+  uri = URI("http://#{settings.api}" + ":#{settings.api_port}" + '/users' )
+  Net::HTTP.post_form(uri, {"n" => params["n"]})
   redirect '/'
 end
 
 post '/users/destroy' do
-  Net::HTTP.post_form(URI(root + '/destroy'),params)
+  uri = URI("http://#{settings.api}" + ":#{settings.api_port}" + '/users/destroy' )
+  puts uri
+  Net::HTTP.post_form(uri,params)
   redirect '/'
 end
-
-
