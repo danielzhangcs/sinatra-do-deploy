b5c87c3b1f4823d4ece782792a58c1db4f5468f5
danielzhangcs <danielzhang@brandeis.edu>
2020-02-25 14:51:17 -0500

    update

--- frontend-sinatra/app.rb
+++ frontend-sinatra/app.rb
@@ -7,12 +7,12 @@ require 'json'
 #   settings.api => "someaddress.ondigitalocean.com"
 set :api, ENV['API_HOST']
 set :api_port, ENV['API_PORT'] || 80
-root = 'http://localhost:4568/users'
+root = '157.245.113.67/users'
 uri = URI(root)
 
 # Show a pretty table of all users... rendered on a html page.
 get '/' do
-  res =  Net::HTTP.get_response(uri)
+  res =  Net::HTTP.get_response(ENV['API_HOST'] + "/users", ENV['API_PORT'])
   @users = JSON.parse(res.body)
   erb :index
 end
