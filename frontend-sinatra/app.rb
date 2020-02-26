9820ae0f466f7262a4565dc806303d4a4c317533
danielzhangcs <danielzhang@brandeis.edu>
2020-02-26 14:08:39 -0500

    update

--- frontend-sinatra/app.rb
+++ frontend-sinatra/app.rb
@@ -13,18 +13,36 @@ set :api_port, ENV['API_PORT'] || 80
 get '/' do
   res =  Net::HTTP.get_response(settings.api, '/users', settings.api_port)
   @users = JSON.parse(res.body)
+
+  # puts res.code
+  # puts res.message
+  # puts res.body
+
   erb :index
 end
 
 post '/users' do
   uri = URI("http://#{settings.api}" + ":#{settings.api_port}" + '/users' )
-  Net::HTTP.post_form(uri, {"n" => params["n"]})
-  redirect '/'
+  input = params["n"]
+  input.strip!
+  input = "1" if input.length == 0
+  res = Net::HTTP.post_form(uri, {"n" => input})
+  code = res.code.to_i
+  if code >= 400
+    erb :error
+  else
+    redirect '/'
+  end
+
 end
 
 post '/users/destroy' do
   uri = URI("http://#{settings.api}" + ":#{settings.api_port}" + '/users/destroy' )
-  puts uri
-  Net::HTTP.post_form(uri,params)
-  redirect '/'
+  res = Net::HTTP.post_form(uri,params)
+  code = res.code.to_i
+  if code >= 400
+    erb :error
+  else
+    redirect '/'
+  end
 end
